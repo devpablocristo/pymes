5a6dcebf62de1029296665b5e8d819fc7e0911e6
devpablocristo <devpablocristo@gmail.com>
2026-04-20 08:53:03 -0300

    fix: unblock backend deploy migrations

--- pymes-core/backend/migrations/runner.go
+++ pymes-core/backend/migrations/runner.go
@@ -10,22 +10,14 @@ import (
 	gormdb "github.com/devpablocristo/core/databases/postgres/go"
 )
 
-const postSchedulingMigrationsTable = "pymes_core_post_scheduling_schema_migrations"
-
 //go:embed *.sql
 var sqlFiles embed.FS
-
-//go:embed all:post_scheduling
-var postSchedulingSQLFiles embed.FS
 
 func Run(db *gorm.DB, logger zerolog.Logger) error {
 	if err := gormdb.GormMigrateUp(db, sqlFiles, "."); err != nil {
 		return err
 	}
 	if err := schedulingmigrations.Run(db); err != nil {
-		return err
-	}
-	if err := gormdb.GormMigrateUp(db, postSchedulingSQLFiles, "post_scheduling", gormdb.WithMigrationsTable(postSchedulingMigrationsTable)); err != nil {
 		return err
 	}
 	logger.Info().Msg("database migrations applied")
